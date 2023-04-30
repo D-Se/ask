f233b0526ef386bbb15d72dff96b514c6c05c05b
D-Se <2869830202@qq.com>
2023-04-30 14:49:40 +0800

    c-level case decision

--- src/ask.c
+++ src/ask.c
@@ -1,25 +1,45 @@
 #include <Rinternals.h>
 #define S SEXP
 
-SEXP rhs(SEXP f, SEXP rho) {
-	switch(Rf_length(f)) {
+S lhs(S fml, S rho) {
+	switch(Rf_length(fml)) {
+		case 1: return fml; // if without else
 		case 2: return R_NilValue;
-		case 3: return Rf_eval(CADDR(f), rho);
+		case 3: return Rf_eval(CADR(fml), rho);
 		default: Rf_error("Malformed `~` in `?`");
 	};
 }
 
-SEXP lhs(SEXP f, SEXP rho) {
-	switch(Rf_length(f)) {
-		case 2: return R_NilValue;
-		case 3: return Rf_eval(CADR(f), rho);
+S rhs(S fml, S rho) {
+	switch(Rf_length(fml)) {
+		case 1: return R_NilValue; // void else, invisible is not in the R API.
+		case 2: return Rf_eval(CADR(fml), rho);
+		case 3: return Rf_eval(CADDR(fml), rho);
 		default: Rf_error("Malformed `~` in `?`");
 	};
+}
+
+SEXP cases(S x, S query, S rho) {
+	int res = 0;
+	if (IS_SCALAR(x, LGLSXP)) {
+		int cond = NA_LOGICAL;
+		cond = *LOGICAL(x);
+		if (cond == NA_LOGICAL) {Rf_error("missing value where T/F needed");}
+		res = cond ? 1 : 2;
+	} else if (TYPEOF(x) == LGLSXP) {
+		int l = LENGTH(x);
+		if (l == 0) {Rf_error("argument is of length zero");}
+		res = l < 1001 ? 3 : 4;
+	} else {
+		res = 5;
+	}
+	return Rf_ScalarInteger(res);
 }
 
 static const R_CallMethodDef entries[] = {
 	{"c_lhs", (DL_FUNC) &lhs, 2},
 	{"c_rhs", (DL_FUNC) &rhs, 2},
+	{"c_cases", (DL_FUNC) &cases, 1},
 	{NULL, NULL, 0}
 };
 
