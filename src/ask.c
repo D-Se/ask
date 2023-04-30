04b54ff4fc7a027c2f3b62fa27036fe0f46516a3
D-Se <2869830202@qq.com>
2023-04-30 15:15:37 +0800

    revert swtich-style ? body

--- src/ask.c
+++ src/ask.c
@@ -19,27 +19,9 @@ S rhs(S fml, S rho) {
 	};
 }
 
-SEXP cases(S x) {
-	int res = 0;
-	if (IS_SCALAR(x, LGLSXP)) {
-		int cond = NA_LOGICAL;
-		cond = *LOGICAL(x);
-		if (cond == NA_LOGICAL) {Rf_error("missing value where T/F needed");}
-		res = cond ? 1 : 2;
-	} else if (TYPEOF(x) == LGLSXP) {
-		int l = LENGTH(x);
-		if (l == 0) {Rf_error("argument is of length zero");}
-		res = l < 1001 ? 3 : 4;
-	} else {
-		res = 5;
-	}
-	return Rf_ScalarInteger(res);
-}
-
 static const R_CallMethodDef entries[] = {
 	{"c_lhs", (DL_FUNC) &lhs, 2},
 	{"c_rhs", (DL_FUNC) &rhs, 2},
-	{"c_cases", (DL_FUNC) &cases, 1},
 	{NULL, NULL, 0}
 };
 
